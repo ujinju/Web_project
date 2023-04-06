693f9f2ce360eb003d6e8d19a6f16d73900e61bf
ujinju <jinju201118@gmail.com>
2023-04-06 21:23:50 +0900

    menu-slide

---
+++ pracitce/menu-slide.js
@@ -0,0 +1,27 @@
+let horizontalUnderLine = document.getElementById('horizontal-underline');
+let horizontalMenus = document.querySelectorAll('nav:first-child a');
+
+horizontalMenus.forEach((menu) =>
+  menu.addEventListener('click', (e) => horizontalIndicator(e))
+);
+
+function horizontalIndicator(e) {
+  horizontalUnderLine.style.left = e.currentTarget.offsetLeft + 'px';
+  horizontalUnderLine.style.width = e.currentTarget.offsetWidth + 'px';
+  horizontalUnderLine.style.top =
+    e.currentTarget.offsetTop + e.currentTarget.offsetHeight + 'px';
+}
+
+let verticalUnderLine = document.getElementById('vertical-underline');
+let verticalMenus = document.querySelectorAll('nav:nth-child(2) a');
+
+verticalMenus.forEach((menu) =>
+  menu.addEventListener('click', (e) => verticalIndicator(e))
+);
+
+function verticalIndicator(e) {
+  verticalUnderLine.style.left = e.currentTarget.offsetLeft + 'px';
+  verticalUnderLine.style.width = e.currentTarget.offsetWidth + 'px';
+  verticalUnderLine.style.top =
+    e.currentTarget.offsetTop + e.currentTarget.offsetHeight + 'px';
+}
